7fec88e3c9c77aa4dd12212ac9bc132f0bf36306
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-10 19:49:27 +0530

    feat(logger): make logger colorful

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,10 @@ import pino from "pino";
 const logger = pino({
     transport: {
         target: "pino-pretty",
-        
+        options: {
+            colorize: true,
+  
+        }
     },
 });
 
