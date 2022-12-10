5cbe3ebb0fd2451ca6fe4efc48c81ba3915f5544
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-10 19:15:37 +0530

    feat(app.js):limit data which coming from request body(json object)

--- src/app.js
+++ src/app.js
@@ -3,5 +3,8 @@ import cors from "cors";
 import 'dotenv/config';
 
 const app =express(); // calling to express
+
 app.use(cors()); // now this backend can access any third party application
+app.use(express.json({limit:"20mb"})); //limit data which coming from request body(json object)
+
 
