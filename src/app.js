38508687ccd63b47eef10d2b54a7c8461927e594
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-10 19:08:43 +0530

    feat(app.js):accessing specific urls by using array

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@ import cors from "cors";
 import 'dotenv/config';
 
 const app =express(); // calling to express
-app.use(cors({origin: "http://localhost:3000"})); // setup cors for our project usefully
+app.use(cors({origin: ["http://localhost:3000","https://google.console.com"]})); // accessing specific urls by using array
+
 
