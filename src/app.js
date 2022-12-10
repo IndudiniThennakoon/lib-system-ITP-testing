eeaf61c2316aa8bc1b55833739d347e832ded999
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-10 19:19:18 +0530

    feat(app.js):start the server

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ import cors from "cors";
 import 'dotenv/config';
 
 const app =express(); // calling to express
+const PORT = process.env.PORT || "8090";
 
 app.use(cors()); // now this backend can access any third party application
 app.use(express.json({limit:"20mb"})); //limit data which coming from request body(json object)
 
-
+app.listen(PORT, () =>{
+    console.log(`🚀 Server is up and running on PORT ${PORT}`);
+});
