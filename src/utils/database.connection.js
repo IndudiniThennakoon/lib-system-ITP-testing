3e012d83fee17d4522aebe5d125a921861384a88
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-11 23:36:23 +0530

    feat(database.connection):connected or not

--- src/utils/database.connection.js
+++ src/utils/database.connection.js
@@ -7,7 +7,15 @@ let database;
 const connect = async () => {
     const MONGODB_URL = process.env.DB_CONNECTION_STRING;
 
+    if (database) return;
 
-    
+    mongoose.connect(MONGODB_URL).then((connection) => {
+        database.connection;
+        logger.info("Database Synced");
+    }).catch((err) => {
+        logger.error(err.message);
+    });
+
+
 
 };
