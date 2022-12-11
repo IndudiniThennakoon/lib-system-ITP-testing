7e57974290957a4d0686f6ab07aa9dbc31b9f54f
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-11 23:35:33 +0530

    feat(database.connection):import logger

--- src/utils/database.connection.js
+++ src/utils/database.connection.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import config from "../config";
+import logger from "./logger";
 
 let database;
 
-const connect = async ()=>{
+const connect = async () => {
     const MONGODB_URL = process.env.DB_CONNECTION_STRING;
+
+
+    
+
 };
